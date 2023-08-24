a727b296cec89bed82cc3fca655042c0387a667a
H0onnn <dukei2017@naver.com>
2023-08-25 00:47:59 +0900

    Feat: AddTodo 컴포넌트 할일 추가 로직 구현

--- src/components/todo/AddTodo.jsx
+++ src/components/todo/AddTodo.jsx
@@ -1,29 +1,49 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { createTodo } from '../../api/todo';
+import { useTodoContext } from '../../context/TodoContext';
+import Input from '../ui/Input';
+import Button from '../ui/Button';
 
-export const AddTodo = () => {
+const AddTodo = () => {
+  const { todoList: prevTodos, handleCreateTodo } = useTodoContext();
+  const [inputData, setInputData] = useState('');
 
-    const [inputData, setInputData] = useState('');
+  const handleInputValueChange = event => {
+    setInputData(event.target.value);
+  };
 
-    const handleChange = (event) => {
-        setInputData(event.target.value);
-    };
+  const handleFormSubmit = async event => {
+    event.preventDefault();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    if (inputData && inputData.trim() === '') {
+      alert('할 일을 입력해주세요.');
+      return;
+    }
 
-        const body = {
-            todo: inputData
-        };
+    try {
+      const newTodo = await createTodo(inputData);
+      handleCreateTodo(prevTodos, newTodo);
+      alert('할 일이 추가되었습니다.');
 
-        // API 호출하는 부분.
+      setInputData('');
+    } catch (error) {
+      alert(error.message);
+    }
+  };
 
-        setInputData('');
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <input data-testid="new-todo-input" placeholder='새롭게 추가할 내용을 입력해주세요.' onChange={handleChange} value={inputData} />
-            <button data-testid="new-todo-add-button">추가</button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <Input
+        placeholder="추가할 항목을 입력해주세요."
+        onChange={handleInputValueChange}
+        value={inputData}
+        data-testid="new-todo-input"
+      />
+      <Button variant="primary" onClick={handleFormSubmit} data-testid="new-todo-add-button">
+        추가
+      </Button>
+    </form>
+  );
 };
+
+export default AddTodo;
