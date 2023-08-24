1be31844c4acb6625b9cd2d5a516aab157c52645
AKOWSPC <akows141@gmail.com>
2023-08-24 21:19:51 +0900

    Feat: 로그인, 회원가입 컴포넌트 구현. Feat: 유효성 검사 Custom Hook 구현.

---
+++ src/hooks/useValidation.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export const useValidation = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const validateEmail = () => {
+        const isValid = email.includes('@');
+        return isValid;
+    };
+
+    const validatePassword = () => {
+        const isValid = password.length >= 8;
+        return isValid;
+    };
+
+    return { email, setEmail, password, setPassword, validateEmail, validatePassword };
+}
+
+export default useValidation;
+  
---
+++ src/pages/auth/SignUpPage.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import useValidation from '../../hooks/useValidation';
+
+export const SignUpPage = () => {
+
+  const { email, setEmail, password, setPassword, validateEmail, validatePassword } = useValidation();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const isSubmitDisabled = !validateEmail() || !validatePassword();
+
+  return (
+
+      <form onSubmit={handleSubmit}>
+        
+        <div>
+          <label htmlFor='email'>Email:</label>
+          <input data-testid="email-input" placeholder='이메일 주소를 입력해주세요' type='text' value={email} onChange={(event) => setEmail(event.target.value)} id='email' />
+        </div>
+
+        <div>
+          <label htmlFor='password'>Password:</label>
+          <input data-testid="password-input" placeholder='비밀번호를 입력해주세요' type='password' value={password} onChange={(event) => setPassword(event.target.value)} id='password' />
+        </div>
+
+        <button data-testid="signup-button" type='submit' disabled={isSubmitDisabled}>제출</button>
+
+      </form>
+
+  );
+}
+
+export default SignUpPage;
